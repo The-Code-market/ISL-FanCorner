1ad9c09896fcabab3278606124a85e4cb4ad8d2a
Vishnu <vishnupothan2@gmail.com>
2020-11-10 12:48:40 +0530

    Account fragment cards added

--- app/src/main/java/com/codemaster/fancorner/Fragment/AccountFragment.java
+++ app/src/main/java/com/codemaster/fancorner/Fragment/AccountFragment.java
@@ -1,66 +1,49 @@
 package com.codemaster.fancorner.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.codemaster.fancorner.BuildConfig;
 import com.codemaster.fancorner.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AccountFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AccountFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AccountFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AccountFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AccountFragment newInstance(String param1, String param2) {
-        AccountFragment fragment = new AccountFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    CardView personalInfoCard, shareCard, rateFeedBackCard;
+    TextView userNameText, logoutText, roleText;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        userNameText = view.findViewById(R.id.userNameText);
+        logoutText = view.findViewById(R.id.logoutText);
+        roleText = view.findViewById(R.id.teamText);
+        personalInfoCard = view.findViewById(R.id.personalInfo);
+        shareCard = view.findViewById(R.id.shareCard);
+        rateFeedBackCard = view.findViewById(R.id.rateFeedBackCard);
+
+        rateFeedBackCard.setOnClickListener(v -> {
+            Intent rateIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id="+ BuildConfig.APPLICATION_ID));
+            startActivity(rateIntent);
+        });
+
+        shareCard.setOnClickListener(v -> {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "ISL Fan Corner.");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID);
+            startActivity(Intent.createChooser(shareIntent, "choose one"));
+        });
+
+        return view;
+
     }
 }

e5f6ce8a48a02405139b479a687b65d90418e26d
Vishnu <vishnupothan2@gmail.com>
2020-11-10 08:47:16 +0530

    MainActivity added with fragments for bottom navigation bar

---
+++ app/src/main/java/com/codemaster/fancorner/MainActivity.java
@@ -0,0 +1,58 @@
+package com.codemaster.fancorner;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.codemaster.fancorner.Fragment.AccountFragment;
+import com.codemaster.fancorner.Fragment.ChatFragment;
+import com.codemaster.fancorner.Fragment.Home;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        setBottomNavigationView();
+        moveToFragment(new Home());
+    }
+
+    public void moveToFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void setBottomNavigationView(){
+       bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.home) {
+                moveToFragment(new Home());
+                return true;
+            }
+            else if(item.getItemId() == R.id.chat) {
+                moveToFragment(new ChatFragment());
+                return true;
+            }
+            else if(item.getItemId() == R.id.account) {
+                moveToFragment(new AccountFragment());
+                return true;
+            }
+            return false;
+        });
+    }
+}
--- app/src/main/java/com/codemaster/fancorner/OTPScreen.java
+++ app/src/main/java/com/codemaster/fancorner/OTPScreen.java
@@ -1,16 +1,32 @@
 package com.codemaster.fancorner;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class OTPScreen extends AppCompatActivity {
 
+    Button verifyBtn;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_o_t_p_screen);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        verifyBtn = findViewById(R.id.verifyBtn);
+
+        verifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainScreenIntent = new Intent(OTPScreen.this, MainActivity.class);
+                startActivity(mainScreenIntent);
+            }
+        });
     }
 }

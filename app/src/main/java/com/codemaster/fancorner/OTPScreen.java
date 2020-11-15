d6c5893a57725a2372a1f0660facef1fd6b505fa
Vishnu <vishnupothan2@gmail.com>
2020-11-15 18:57:49 +0530

    Login firebase connected

--- app/src/main/java/com/codemaster/fancorner/OTPScreen.java
+++ app/src/main/java/com/codemaster/fancorner/OTPScreen.java
@@ -1,17 +1,40 @@
 package com.codemaster.fancorner;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.chaos.view.PinView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class OTPScreen extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
+    String verificationCodeBySystem;
+    String phoneNumberStr;
     Button verifyBtn;
+    PinView OTPCodePinView;
+    ProgressBar progressBar;
+
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +42,83 @@ public class OTPScreen extends AppCompatActivity {
         setContentView(R.layout.activity_o_t_p_screen);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
+        //initialization
         verifyBtn = findViewById(R.id.verifyBtn);
+        OTPCodePinView = findViewById(R.id.pinView);
+        progressBar = findViewById(R.id.progressBar);
 
-        verifyBtn.setOnClickListener(new View.OnClickListener() {
+        //get intent values
+        phoneNumberStr = getIntent().getStringExtra("PhoneNumber");
+        Log.i("here otp", phoneNumberStr);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        sendVerificationCode(phoneNumberStr);
+
+        //verifyBtn click listener
+        verifyBtn.setOnClickListener(v -> {
+            String code = OTPCodePinView.getText().toString();
+            if (code.isEmpty() || code.length() < 6) {
+                OTPCodePinView.setError("Wrong OTP..");
+                OTPCodePinView.requestFocus();
+                return;
+            }
+            progressBar.setVisibility(View.VISIBLE);
+            verifyCode(code);
+        });
+    }
+
+    private void sendVerificationCode(String phoneNo) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNo)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    // Initialize phone auth callbacks
+    private final PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationCodeBySystem = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential credential) {
+            String code = credential.getSmsCode();
+            if (code != null) {
+                progressBar.setVisibility(View.VISIBLE);
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(OTPScreen.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private void verifyCode(String codeByUser) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCodeBySystem, codeByUser);
+        signInTheUserByCredential(credential);
+    }
+
+    private void signInTheUserByCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential).addOnCompleteListener(OTPScreen.this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View v) {
-                Intent mainScreenIntent = new Intent(OTPScreen.this, ChatScreen.class);
-                startActivity(mainScreenIntent);
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                    mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(mainIntent);
+                } else {
+                    Toast.makeText(OTPScreen.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/codemaster/fancorner/SignInScreen.java
+++ app/src/main/java/com/codemaster/fancorner/SignInScreen.java
@@ -1,20 +1,29 @@
 package com.codemaster.fancorner;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class SignInScreen extends AppCompatActivity {
-FirebaseAuth mAuth;
+
     TextInputEditText mobileNumberTextInput;
     Button sendOTPBtn;
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -24,12 +33,20 @@ FirebaseAuth mAuth;
         setContentView(R.layout.activity_sign_in);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
+        //initialization
         mobileNumberTextInput = findViewById(R.id.mobileNumberTextInput);
         sendOTPBtn = findViewById(R.id.sendOtpButton);
 
+        //sendOtp button clicked listener
         sendOTPBtn.setOnClickListener(v -> {
-            Intent otpScreenIntent = new Intent(SignInScreen.this, OTPScreen.class);
-            startActivity(otpScreenIntent);
+            Intent OTpScreenIntent = new Intent(SignInScreen.this, OTPScreen.class);
+            OTpScreenIntent.putExtra("PhoneNumber", "+91" + mobileNumberTextInput.getText().toString());
+            Log.i("here Sign In", String.valueOf(mobileNumberTextInput.getText()));
+            startActivity(OTpScreenIntent);
         });
+
     }
+
+
+
 }

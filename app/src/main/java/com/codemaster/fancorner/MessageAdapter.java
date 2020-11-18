4664a277d59392240c95082911dbbe1428347356
Vishnu <vishnupothan2@gmail.com>
2020-11-18 14:11:01 +0530

    Chat and sign in merged

--- app/src/main/java/com/codemaster/fancorner/MessageAdapter.java
+++ app/src/main/java/com/codemaster/fancorner/MessageAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.codemaster.fancorner.model.Messages;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;

844f6acf51acc253555ce9ae110997e1fb70cc85
fajarcp <fajarfaju82@gmail.com>
2020-11-26 11:53:41 +0530

    today new

--- app/src/main/java/com/codemaster/fancorner/SignInScreen.java
+++ app/src/main/java/com/codemaster/fancorner/SignInScreen.java
@@ -6,17 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.codemaster.fancorner.SharedPreference.SharedPreference;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class SignInScreen extends AppCompatActivity {
     TextInputEditText mobileNumberTextInput;
